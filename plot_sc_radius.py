53d132f92efa84d7519a25313c40be32301a13dd
RuQing Xu <rbh1998@mail.ustc.edu.cn>
2018-07-17 00:17:16 +0900

    Plot SC Radius Now Shows R=Rc Instead of R>Rc

--- plot_sc_radius.py
+++ plot_sc_radius.py
@@ -15,20 +15,24 @@ for fnm in fnm_list:
     i = 0
     fid = open(fnm)
     fln = fid.readline().strip()
+    sc_tmp = np.zeros(r_set)
     while (fln != ''):
         fln = re.sub(' +', ' ', fln).split(' ')
         if (r_set == 0):
             r_list.append(float(fln[0]))
-            sc_list.append([ float(fln[1]), \
-                             float(fln[1]) ** 2 ])
+            sc_list.append([ float(fln[1]), 0 ])
         else:
-            sc_list[i] += [ float(fln[1]), \
-                            float(fln[1]) ** 2 ]
+            sc_tmp[i] = float(fln[1])
             i += 1
         fln = fid.readline().strip()
     if (r_set == 0):
-        r_set = 1
+        r_set = len(r_list)
         sc_list = np.array(sc_list)
+        sc_tmp = np.array(sc_list[:, 0])
+        sc_list[:, :] = 0
+    for j in range(r_set - 1):
+        sc_list[j, 0] +=  sc_tmp[j] - sc_tmp[j + 1]
+        sc_list[j, 1] += (sc_tmp[j] - sc_tmp[j + 1]) ** 2
 sc_list = sc_list/total_bin
 sc_list[:, 1] = np.sqrt(sc_list[:, 1] - sc_list[:, 0] ** 2) #/np.sqrt(total_bin)
 
