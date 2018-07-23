d1abf20f7d96bb1b344798d019e47a5a8d5803f2
RuQing Xu <rbh1998@mail.ustc.edu.cn>
2018-07-23 23:58:12 +0900

    Small Fix In Simplified Mode

--- opt_generators/sc_classes.hh
+++ opt_generators/sc_classes.hh
@@ -167,7 +167,8 @@ struct sc_corr
                 system.r(rb, b);
                 system.r(ri, i);
                 for (auto ii = 0; ii < rc_count; ii++)
-                    if (ri[0] - rb[0] == rx_lst[ii] && ri[1] - rb[1] == ry_lst[ii]) {
+                    if (ri[0] - rb[0] + system.w % system.w == rx_lst[ii] &&
+                        ri[1] - rb[1] + system.w % system.w == ry_lst[ii]) {
 #pragma omp critical
                         values[ii] += x * sign / (2. * system.n);
                         printf("%d %s %d %s %d %s %d %s %d %d %f\n",
