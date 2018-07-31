b7b911efb5dd5690003dfaa0877be8219e661eae
RuQing Xu <roger.hsu.sol@icloud.com>
2018-07-31 18:08:41 +0900

    UHF Initialize Script Only for >= Ver. 1.0

--- Scripts/UHF_Init.sh
+++ Scripts/UHF_Init.sh
@@ -6,25 +6,18 @@ for i in 16 14 12 10 8; do
 	cd U10N$i
 	cat >> modpara.def << EOF
 --------------------
-2Sz            0
 Mix            0.5
 EPS            20
 IterationMax   1000
 --------------------
 EOF
 	sed -i '.bak' '/.*NSRCG.*/d' modpara.def
-	sed -i '.bak' '/.*Ncond.*/d' modpara.def
 	$mVMC_ROOT/src/ComplexUHF/UHF namelist.def
-	cat >> namelist.def << EOF
-InOrbital zqp_APOrbital_opt.dat
-EOF
-	cat >> modpara.def << EOF
-NSRCG 0
-Ncond $i
-EOF
+	echo ' InOrbital zqp_APOrbital_opt.dat' >> namelist.def
 	sed -i '.bak' '/.*Mix.*/d' modpara.def
 	sed -i '.bak' '/.*EPS.*/d' modpara.def
 	sed -i '.bak' '/.*IterationMax.*/d' modpara.def
+	echo ' NSRCG 0' >> modpara.def
 	cd ..
 done
 
