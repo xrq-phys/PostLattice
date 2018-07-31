768ca132278b1f4503d4f34807bfff3655a930f7
RuQing Xu <roger.hsu.sol@icloud.com>
2018-07-31 18:08:19 +0900

    Updated VMC Path for My Own Convenience

--- Scripts/VMC_Path.sh
+++ Scripts/VMC_Path.sh
@@ -1,6 +1,7 @@
 # VMC Variables
 
-mVMC_ROOT=~/Projects/mVMC             # Your path to VMC >=1.0 Build
+mVMC_ROOT=~/Projects/mVMC             # Your path to mVMC >=1.0 Build
+HPhi_ROOT=~/Projects/CMakeBuilds/HPhi # Your path to HPhi >=3.0 Build
 anlys_ROOT=~/Projects/PyLattice       # Your path to the PyLattice analysis program
 
 export OMP_NUM_THREADS=2
