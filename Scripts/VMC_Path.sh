965be5bd2ed372506e6c537a9a4699dd4082abf0
RuQing Xu <roger.hsu.sol@icloud.com>
2018-07-31 14:09:11 +0900

    Change Path For my Own Convenience

--- Scripts/VMC_Path.sh
+++ Scripts/VMC_Path.sh
@@ -1,6 +1,6 @@
 # VMC Variables
 
-mVMC_ROOT=~/Projects/CMakeBuilds/mVMC # Your path to VMC >=1.0 Build
+mVMC_ROOT=~/Projects/mVMC             # Your path to VMC >=1.0 Build
 anlys_ROOT=~/Projects/PyLattice       # Your path to the PyLattice analysis program
 
 export OMP_NUM_THREADS=2
