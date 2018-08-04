a9a6f5fd7d23bf128c5563f55e315952fd807487
RuQing Xu <roger.hsu.sol@icloud.com>
2018-08-04 17:47:43 +0900

    GF Generator

---
+++ Tools_CC/green_gen.cc
@@ -0,0 +1,70 @@
+#include "green_gen.hh"
+#include <cassert>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+// Buffer Filename
+#ifndef tm_fnm
+#define tm_fnm ("/tmp/greentwo.tmp")
+#endif
+
+void write_ca(const char *ca_fnm, lattice::lattice system)
+{
+    std::fstream ca_fid(ca_fnm, std::fstream::out);
+
+    ca_fid << "=============================" << std::endl
+           << " CiAj " << system.n * system.n << std::endl
+           << "=============================" << std::endl
+           << " CiAj Full ==================" << std::endl
+           << "=============================" << std::endl;
+    for (int i = 0; i < system.n; i++)
+        for (int j = 0; j < system.n; j++) {
+            ca_fid << std::setw(6) << i << ' ' << 0 << ' ' << j << ' ' << 0 << std::endl
+                   << std::setw(6) << i << ' ' << 1 << ' ' << j << ' ' << 1 << std::endl;
+        }
+    ca_fid.close();
+}
+
+void write_caca(const char *caca_fnm, lattice::lattice system)
+{
+    std::fstream tmop_fid(tm_fnm, std::fstream::out);
+    int nopr = 0;
+    
+    for (int i = 0; i < system.n; i++)
+        for (int j = 0; j < system.n; j++)
+            for (int k = 0; k < system.n; k++)
+                for (int l = 0; l < system.n; l++) {
+                    if (i == j && k == l) {
+                        tmop_fid << std::setw(6)
+                               << i << ' ' << 0 << ' ' << j << ' ' << 0 << ' ' << k << ' ' << 0 << ' ' << l << ' ' << 0 << std::endl 
+                               << i << ' ' << 1 << ' ' << j << ' ' << 1 << ' ' << k << ' ' << 1 << ' ' << l << ' ' << 1 << std::endl
+                               << i << ' ' << 1 << ' ' << j << ' ' << 1 << ' ' << k << ' ' << 0 << ' ' << l << ' ' << 0 << std::endl
+                               << i << ' ' << 0 << ' ' << j << ' ' << 0 << ' ' << k << ' ' << 1 << ' ' << l << ' ' << 1 << std::endl;
+                        nopr += 4;
+                    } else if (system.nn[i][k] && system.nn[j][l]) {
+                        tmop_fid << std::setw(6)
+                               << i << ' ' << 1 << ' ' << j << ' ' << 1 << ' ' << k << ' ' << 0 << ' ' << l << ' ' << 0 << std::endl
+                               << i << ' ' << 0 << ' ' << j << ' ' << 0 << ' ' << k << ' ' << 1 << ' ' << l << ' ' << 1 << std::endl;
+                        nopr += 2;
+                    }
+                }
+    tmop_fid.close();
+    
+    std::fstream tmin_fid(tm_fnm);
+    std::fstream caca_fid(caca_fnm, std::fstream::out);
+    caca_fid << "=============================" << std::endl
+             << "   CiAjCkAlSCAF      " << nopr << std::endl
+             << "=============================" << std::endl
+             << "== SC/AF Correlation ========" << std::endl
+             << "=============================" << std::endl;
+    std::string io_buffer;
+    while (true) {
+        std::getline(tmin_fid, io_buffer);
+        if (tmin_fid.eof()) 
+            break;
+        caca_fid << io_buffer << std::endl;
+    }
+    tmin_fid.close();
+    caca_fid.close();
+}
---
+++ Tools_CC/green_gen.hh
@@ -0,0 +1,21 @@
+/**
+ * @file green_gen.hh
+ * Green function generator.
+ */
+#include "lattice.hh"
+
+/**
+ * @brief Write 1-body Green function definition used by AF/SC analysis.
+ * 
+ * @param ca_fnm 1-body Green function definition filename.
+ * @param system Lattice system object.
+ */
+void write_ca(const char *ca_fnm, lattice::lattice system);
+
+/**
+ * @brief Write 2-body Green function definition used by AF/SC analysis.
+ * 
+ * @param caca_fnm 2-body Green function definition filename.
+ * @param system Lattice system object.
+ */
+void write_caca(const char *caca_fnm, lattice::lattice system);
