cf99a0e4a49828b7adf3666865897bd15874b662
RuQing Xu <roger.hsu.sol@icloud.com>
2018-08-04 17:47:35 +0900

    Spin-Structure Operator

--- Tools_CC/operators.cc
+++ Tools_CC/operators.cc
@@ -1,4 +1,43 @@
 #include "operators.hh"
+#include <cstring>
+#include <ccomplex>
+#include <cmath>
+
+// {
+// These are Tool Functions.
+// TODO: Put it somewhere else.
+inline double inner_r_qidx(double *r, int *qidx, const int ndim)
+{
+    double sum = 0;
+    for (int i = 0; i < ndim; i++)
+        sum += M_2_PI * qidx[i] * r[i];
+    return sum;
+}
+
+inline int idx_nd(const int *iqpt, const int *nqpt, const int ndim)
+{
+    int idx = 0, nelem = 1;
+    for (int i = ndim - 1; i >=0; i--) {
+        idx += iqpt[i] * nelem;
+        nelem *= nqpt[i];
+    }
+    return idx;
+}
+
+void allocate_qpoints(int **qpts, const int *nqpt, int *iqpt, const int dim, const int ndim)
+{
+    if (dim < ndim) {
+        for (int i = 0; i < nqpt[dim]; i++) {
+            iqpt[dim] = i;
+            allocate_qpoints(qpts, nqpt, iqpt, dim + 1, ndim);
+        }
+    } else {
+        int idx = idx_nd(iqpt, nqpt, ndim);
+        qpts[idx] = new int[ndim];
+        std::memcpy(qpts[idx], iqpt, ndim * sizeof(int));
+    }
+}
+// }
 
 void operators::sc_corr::measure(int a, int sa, int b, int sb, 
                                  int i, int si, int j, int sj, double x)
@@ -26,5 +65,44 @@ void operators::sc_corr::measure(int a, int sa, int b, int sb,
             if (rmin == system.r_c[ii])
 #pragma omp critical
             values[ii] += x * sign / (2. * system.n);
+    }
+}
+
+operators::spin_struct::spin_struct(lattice::lattice system_i, int *ndiv)
+: operators::operators(), system(system_i) 
+{
+    // Total number of q-points.
+    n_points = 1;
+    for (int i = 0; i < system.dim; i++) 
+        n_points *= ndiv[i];
+
+    values = new double[n_points];
+    points = new int*[n_points];
+    int qpt_buff[system.dim];
+    allocate_qpoints(points, ndiv, qpt_buff, 0, system.dim);
+}
+
+void operators::spin_struct::measure(int i, int si, int j, int sj, int k, int sk, int l, int sl, double x)
+{
+    if (i != j || k != l) 
+        return;
+    
+    // Si \cdot Sk.
+    int spin_part = (pauli_x[si][sj] * pauli_x[sk][sl] 
+                   + pauli_z[si][sj] * pauli_z[sk][sl] 
+                   - pauli_y[si][sj] * pauli_y[sk][sl]) / 4.;
+
+    // exp(r * q) == cos(r * q) / 2 (by symmetry).
+    double *dr = new double[system.dim],
+           *r1 = new double[system.dim],
+           *r2 = new double[system.dim];
+    system.r(r1, i);
+    system.r(r2, k);
+    for (int i = 0; i < system.dim; i++)
+        dr[i] = r1[i] - r2[i];
+    for (int i = 0; i < n_points; i++) {
+        double space_part = 1 / (6. * system.n * system.n) *
+                            std::cos(inner_r_qidx(dr, points[i], system.dim));
+        values[i] += spin_part * space_part * x;
     }
 }
--- Tools_CC/operators.hh
+++ Tools_CC/operators.hh
@@ -8,21 +8,17 @@
 namespace operators
 {
     /**
-     * @brief Superconductive correlation function.
+     * @brief Operator base class.
      */
-    struct sc_corr
+    struct operators
     {
-        lattice::lattice &system;
-        char form; ///< Wave form factor of superconductivity: s, d or p.
-        int rc_count; ///< Number of R_c's to use.
-        double *values;
+        double *values; ///< Measured values.
 
-        sc_corr(lattice::lattice &system_i, const int rc_count_i, const char form_i)
-        : system(system_i), rc_count(rc_count_i), form(form_i)
-        { values = new double[rc_count]; for (int i = 0; i < rc_count; i++) values[i] = 0; }
+        operators()
+        : values(nullptr) { }
 
-        ~sc_corr()
-        { delete[] values; }
+        ~operators()
+        { if (values != nullptr) delete[] values; }
 
         /**
          * @brief Do measurement.
@@ -35,9 +31,30 @@ namespace operators
          * @param sb Spin index.
          * @param j  Site index.
          * @param sj Spin index.
-         * @param x  < c c a a >.
+         * @param x  < c c a a > or < c a c a > depending on form of the operator.
          */
-        void measure(int a, int sa, int b, int sb, int i, int si, int j, int sj, double x);
+        virtual void measure(int a, int sa, int b, int sb, int i, int si, int j, int sj, double x);
+    };
+
+    /**
+     * @brief Superconductive correlation function.
+     */
+    struct sc_corr
+    : operators
+    {
+        lattice::lattice &system;
+        char form; ///< Wave form factor of superconductivity: s, d or p.
+        int rc_count; ///< Number of R_c's to use.
+
+        sc_corr(lattice::lattice &system_i, const int rc_count_i, const char form_i)
+        : operators::operators(), system(system_i), rc_count(rc_count_i), form(form_i)
+        { values = new double[rc_count]; for (int i = 0; i < rc_count; i++) values[i] = 0; }
+
+        /**
+         * @brief Implement measurement.
+         * @param x < c c a a >
+         */
+        virtual void measure(int a, int sa, int b, int sb, int i, int si, int j, int sj, double x) override;
 
         /**
          * @brief Validation if a term MIGHT have contribution to SC correlation.
@@ -56,16 +73,35 @@ namespace operators
         { return si != sj && sa != sb && system.nn[a][b] && system.nn[i][j]; }
     };
 
+    /**
+     * @brief Spin structure.
+     */
     struct spin_struct
+    : operators
     {
         lattice::lattice &system;
-        int* points; ///< Q-poitns.
-        double *values; /// Measured values.
+        int n_points;
+        int **points; ///< Q-points.
         // Pauli matrices.
         int pauli_x[2][2];
         int pauli_y[2][2];
         int pauli_z[2][2];
 
-        // NB: USE DOUBLE R ROUTINES WHEN EVALUATING
+        /**
+         * @brief Construct a new spin struct object.
+         * 
+         * @param system_i System.
+         * @param ndiv Number of q-points in each dimension.
+         */
+        spin_struct(lattice::lattice system_i, int *ndiv);
+
+        ~spin_struct()
+        { for (int i = 0; i < n_points; i++) delete[] points[i]; delete[] points; }
+
+        /**
+         * @brief Implement measurement.
+         * @param x < c a c a >
+         */
+        virtual void measure(int a, int sa, int b, int sb, int i, int si, int j, int sj, double x) override;
     };
 }
