599bfa2000388b1dfcdf579a439ca9dd026e0164
RuQing Xu <roger.hsu.sol@icloud.com>
2018-08-06 17:05:57 +0900

    UHF Has Completed Lookup

--- Scripts/UHF_Init.sh
+++ Scripts/UHF_Init.sh
@@ -3,21 +3,24 @@
 source VMC_Path.sh
 
 for i in ./*.dir; do
-	cd $i
-	cat >> modpara.def << EOF
+	if [ ! -e zqp_APOrbital_opt.dat ]; then
+		cd $i
+		echo "Working on $i"
+		cat >> modpara.def << EOF
 --------------------
 Mix            0.4
 EPS            10
 IterationMax   20000
 --------------------
 EOF
-	sed -i '.bak' '/.*NSRCG.*/d' modpara.def
-	$mVMC_ROOT/src/ComplexUHF/UHF namelist.def
-	echo ' InOrbital zqp_APOrbital_opt.dat' >> namelist.def
-	sed -i '.bak' '/.*Mix.*/d' modpara.def
-	sed -i '.bak' '/.*EPS.*/d' modpara.def
-	sed -i '.bak' '/.*IterationMax.*/d' modpara.def
-	echo ' NSRCG 0' >> modpara.def
-	cd ..
+		sed -i '.bak' '/.*NSRCG.*/d' modpara.def
+		$mVMC_ROOT/src/ComplexUHF/UHF namelist.def
+		echo ' InOrbital zqp_APOrbital_opt.dat' >> namelist.def
+		sed -i '.bak' '/.*Mix.*/d' modpara.def
+		sed -i '.bak' '/.*EPS.*/d' modpara.def
+		sed -i '.bak' '/.*IterationMax.*/d' modpara.def
+		echo ' NSRCG 0' >> modpara.def
+		cd ..
+	fi
 done
 
