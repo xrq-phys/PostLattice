9a41a96602ebe46a309340b828bcfdad5e9bdf4a
RuQing Xu <rbh1998@mail.ustc.edu.cn>
2019-07-31 10:58:31 +0800

    Removed Old Run Scripts

--- Scripts/VMC_Path.sh
+++
@@ -1,7 +0,0 @@
-# VMC Variables
-
-mVMC_ROOT=~/Projects/mVMC             # Your path to mVMC >=1.0 Build
-HPhi_ROOT=~/Projects/CMakeBuilds/HPhi # Your path to HPhi >=3.0 Build
-anlys_ROOT=~/Projects/PostLattice     # Your path to the PostLattice analysis program
-
-export OMP_NUM_THREADS=2
